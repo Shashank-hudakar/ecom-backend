47c755bde1c598ecdffbf354764254b4efd8750e
Shashank-hudakar <shambhulingappahudakar@gmail.com>
2025-05-18 11:16:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin:`http://localhost:5173`
+    origin:`https://ecom-frontend-dun.vercel.app`
 }));
 app.use(cookieParser());
 app.get("/", (req, res) => {
